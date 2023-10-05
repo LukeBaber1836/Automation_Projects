5de1fbbb0a6573861079015a2907e1756d17642a
LukeBaber1836 <luke.baber1@gmail.com>
2023-10-05 14:57:09 -0500

    Finished news automation

--- Automation_With_APIs/News_Automation_API.py
+++ Automation_With_APIs/News_Automation_API.py
@@ -1,25 +1,28 @@
 import requests
+from datetime import datetime as dt
 
 # Global variables
-API_key = "1041069513544acebf98f56359e28b31"
-Language = "en"     # English is default
+API_key = "9d8f995de0de6283e9cbb0e48d00f94b"
+data_file = open ("data.txt", "a")
 
 # Gets the desired news data based off user input
-def get_news(topic, from_date, to_date, language = Language, api_key = API_key):
-    url = f"https://newsapi.org/v2/everything?qInTitle={topic}%20market&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={API_key}"
+def get_weather(city_name, api_key = API_key):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_key}"
     req = requests.get(url)
     contents = req.json()
-    articles = contents['articles']
-    results = []
 
-    for article in articles:
-        results.append(f"Title \n' {article['title']}, '\n Description \n', {article['description']}")
+    for data in contents['list']:
+        # add city, time, temperature, and condition to .txt file
+        time_stamp = dt.fromtimestamp(data['dt']) # Convert timestamp to date
+        temp = round(9 / 5 * (float(data['main']['temp']) - 273.15) + 32) # Kelvin to Fahrenheit
+        condition = data['weather'][0]['description']
+        data_file.write(f"{city_name}   {time_stamp}   {temp}   {condition}\n") # Write data to .txt file
+    #data_file.close()
+    data_file.read()
+
+def __main__():
+    get_weather(city_name= "tyler")
     
-    return results
 
 
-#def __main__():
-print(repr((get_news(topic= "space",from_date= "2023-09-01", to_date= "2023-09-15"))).replace(r'\n', '\n'))
-
-
-#print(__main__)
+__main__()
