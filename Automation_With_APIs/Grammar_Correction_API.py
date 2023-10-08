05c338146951b8973d94ee5f2ff8654f93a75cc4
LukeBaber1836 <luke.baber1@gmail.com>
2023-10-08 17:30:45 -0500

    Grammar Correction finished

---
+++ Automation_With_APIs/Grammar_Correction_API.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+# Uses langauge tool API for spell check correction
+def spell_check(sentence):
+    url = "https://api.languagetool.org/v2/check"
+    data = {
+        'text' : sentence,
+        'language' : 'auto'
+    }
+    response = requests.post(url, data=data).json()
+    #print(json.dumps(response, indent= 2))
+
+    for x in range(len(response['matches'])):
+        correct_word = response['matches'][x]['replacements'][0]['value']
+        # Use these values for inserting ne word at correct location
+        start = int(response['matches'][x]['offset'])
+        end = int(response['matches'][x]['length']) + start
+        old_word = data['text'][start:end]
+        sentence = sentence.replace(old_word, correct_word)
+    
+    return sentence
+
+def get_input():
+    value = input("Type a sentence: ")
+    return value
+
+def __main__():
+    user_input = get_input()
+    correction = spell_check(user_input)
+    print("Corrected sentence: " + correction)
+
+__main__()
