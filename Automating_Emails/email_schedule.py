307257b6c5e1206314d7453577910aadc8e3bd36
LukeBaber1836 <luke.baber1@gmail.com>
2023-10-23 17:32:56 -0500

    Automated emails

---
+++ Automating_Emails/email_schedule.py
@@ -0,0 +1,24 @@
+import yagmail
+import os
+import time
+from datetime import datetime as dt
+
+sender = 'lbcoding.1@gmail.com'
+PASSWORD = os.environ.get('lbcoding_pw')
+
+receiver = 'gxnsahsqe@emlpro.com'   #Temp email from: https://dropmail.me/en/
+num = 0
+while True:
+    now = dt.now()
+    # Sends an email at 5:29 CDT
+    if now.hour == 17 and now.minute == 29:
+        subject = f"This is you daily email {num}"
+        contents = f"""
+            Here is a random email that I am sending with Python!
+            Hi from {num}!
+        """
+        yag = yagmail.SMTP(user=sender, password=PASSWORD)
+        yag.send(to=receiver, subject=subject, contents=contents)
+        num +=1
+        print("Email sent!")
+        time.sleep(60)
