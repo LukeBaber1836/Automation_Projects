d0e8e1990e79de32bb4639a1b322a6f367ae7bba
LukeBaber1836 <luke.baber1@gmail.com>
2023-11-20 15:38:42 -0600

    updated test app

--- Modern_Python_Tools/streamlit_example.py
+++ Modern_Python_Tools/streamlit_example.py
@@ -1,7 +1,23 @@
 import streamlit as st
+import pandas
+
+# Access online at: https://lukebaber-test-app.streamlit.app/
+
+#Through a graph on streamlit
+data = {
+    'Series_1':[1,3,4,5,7],
+    'Series_2':[10,30,40,100,250]
+}
+df = pandas.DataFrame(data)
 
 st.title("My first project")
 st.subheader("Automating things with Python!")
 st.write('''This is my first app.
 Enjoy it!
 ''')
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+my_slider = st.slider('Celsius')
+st.write(my_slider, 'in Fahrenheit is', my_slider * 9/5 + 32)
